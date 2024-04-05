014ee3800421d606fc8dbec2c92cc996f7f12ccd
Andrew Baumheckel <andrew.baumheckel@gmail.com>
2024-04-05 14:09:19 -0700

    attached login form to vars

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,37 +1,57 @@
 <script setup>
-const fs = window.electron.fs;
+import { ref } from "vue";
 
-// console.log(fs.readFileSync("./index.js", { encoding: "utf8" }));
+// const fs = window.electron.fs;
 
-import io from "socket.io-client";
+// // console.log(fs.readFileSync("./index.js", { encoding: "utf8" }));
 
-// Connect to the Socket.IO server with the self-signed certificate
-const socket = io("https://localhost", {});
+// import io from "socket.io-client";
 
-// Event handler for disconnection
-socket.on("disconnect", () => {
-	console.log("Disconnected from server");
+// // Connect to the Socket.IO server with the self-signed certificate
+// const socket = io("https://localhost", {});
+
+// // Event handler for disconnection
+// socket.on("disconnect", () => {
+// 	console.log("Disconnected from server");
+// });
+
+// // Function to handle incoming messages from the server
+// socket.on("chat message", function (msg) {
+// 	console.log("Received message:", msg);
+// });
+
+// // Function to send messages to the server
+// function sendMessage(message) {
+// 	socket.emit("chat message", message);
+// }
+
+const loginForm = ref(true);
+
+const loginFormData = ref({
+	username: "",
+	password: "",
+	repeatPassword: "",
+	address: "",
+	port: 443,
+	rememberMe: false,
 });
 
-// Function to handle incoming messages from the server
-socket.on("chat message", function (msg) {
-	console.log("Received message:", msg);
-});
-
-// Function to send messages to the server
-function sendMessage(message) {
-	socket.emit("chat message", message);
+const formError = ref("");
+function switchForms() {
+	loginForm.value = !loginForm.value;
 }
 
 // Example usage: sending a message to the server
-sendMessage("Hello, server!");
+// sendMessage("Hello, server!");
 </script>
 
 <template>
 	<header>
 		<div class="row justify-content-center">
-			<div class="col-2 justify-content-center">
-				<h2>Login</h2>
+			<div class="col-5" style="display: flex; justify-content: center">
+				<button @click="switchForms()" type="submit" class="btn btn-primary">
+					Login / Sign Up
+				</button>
 			</div>
 		</div>
 	</header>
@@ -40,47 +60,64 @@ sendMessage("Hello, server!");
 		<div class="row justify-content-center">
 			<div class="col-6">
 				<label class="form-label">Server Address</label>
-				<input type="text" class="form-control" />
+				<input v-model="loginFormData.address" type="text" class="form-control" />
 			</div>
 
 			<div class="col-2">
 				<label class="form-label">Port</label>
-				<input type="text" class="form-control" />
+				<input v-model="loginFormData.port" type="number" class="form-control" />
 			</div>
 		</div>
 
 		<div class="row justify-content-center">
 			<div class="col-8">
 				<label>Username</label>
-				<input type="text" class="form-control" />
+				<input v-model="loginFormData.username" type="text" class="form-control" />
 			</div>
 		</div>
 
 		<div class="row justify-content-center">
 			<div class="col-8">
 				<label>Password</label>
-				<input type="password" class="form-control" />
+				<input
+					v-model="loginFormData.password"
+					type="password"
+					class="form-control"
+				/>
 			</div>
 		</div>
-		<div class="row justify-content-center">
+		<div v-show="!loginForm" class="row justify-content-center">
 			<div class="col-8">
 				<label>Repeat Password</label>
-				<input type="password" class="form-control" />
+				<input
+					v-model="loginFormData.repeatPassword"
+					type="password"
+					class="form-control"
+				/>
+			</div>
+		</div>
+
+		<div v-show="formError.length > 0" class="row justify-content-center">
+			<div class="col-4 formError">
+				{{ formError }}
 			</div>
 		</div>
 
 		<div class="row align-items-center justify-content-center">
 			<div class="col-2">
-				<button type="submit" class="btn btn-primary">Login</button>
+				<button v-show="loginForm" class="btn btn-primary">Login</button>
+				<button v-show="!loginForm" class="btn btn-primary">Sign Up</button>
 			</div>
 			<div class="col-4">
-				<label
-					style="margin-right: 10px"
-					class="form-check-label"
-					for="exampleCheck1"
+				<label style="margin-right: 10px" class="form-check-label"
 					>Remember me</label
 				>
-				<input type="checkbox" class="form-check-input" id="exampleCheck1" />
+				<input
+					v-model="loginFormData.rememberMe"
+					type="checkbox"
+					class="form-check-input"
+					id="exampleCheck1"
+				/>
 			</div>
 			<div class="col-2"></div>
 		</div>
