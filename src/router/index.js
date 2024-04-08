cc3e2034e05be989ed673300748709851dde4621
Andrew Baumheckel <andrew.baumheckel@gmail.com>
2024-04-07 19:09:32 -0700

    temp

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import loginView from "@/views/login/login.vue";
+import homeView from "@/views/home/home.vue";
 
 const router = createRouter({
 	history: createWebHashHistory(),
 	routes: [
 		{
 			path: "/",
-			name: "loginView",
-			component: loginView,
+			name: "homeView",
+			component: homeView,
 		},
 		{
 			path: "/about",
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,9 +3,9 @@ import { ref } from "vue";
 
 // const fs = window.electron.fs;
 
-import { register } from '../../../vue-advanced-chat/dist/vue-advanced-chat.mjs'
+import { register } from "../../../vue-advanced-chat/dist/vue-advanced-chat.mjs";
 //https://www.npmjs.com/package/vue-advanced-chat
-  register()
+register();
 
 import io, { connect } from "socket.io-client";
 
@@ -32,124 +32,129 @@ async function connectToServer(url, port) {
 	return socket;
 }
 
-
-const currentUserId = ref('1234')
-const  rooms = ref([
-  {
-    roomId: '1',
-    roomName: 'Room 1',
-    avatar: 'assets/imgs/people.png',
-    unreadCount: 4,
-    index: 3,
-    lastMessage: {
-      _id: 'xyz',
-      content: 'Last message received',
-      senderId: '1234',
-      username: 'John Doe',
-      timestamp: '10:20',
-      saved: true,
-      distributed: false,
-      seen: false,
-      new: true
-    },
-    users: [
-      {
-        _id: '1234',
-        username: 'John Doe',
-        avatar: 'assets/imgs/doe.png',
-        status: {
-          state: 'online',
-          lastChanged: 'today, 14:30'
-        }
-      },
-      {
-        _id: '4321',
-        username: 'John Snow',
-        avatar: 'assets/imgs/snow.png',
-        status: {
-          state: 'offline',
-          lastChanged: '14 July, 20:00'
-        }
-      }
-    ],
-    typingUsers: [ 4321 ]
-  }
-])
-const messages = ref([
-  {
-    _id: '7890',
-    indexId: 12092,
-    content: 'Message 1',
-    senderId: '1234',
-    username: 'John Doe',
-    avatar: 'assets/imgs/doe.png',
-    date: '13 November',
-    timestamp: '10:20',
-    system: false,
-    saved: true,
-    distributed: true,
-    seen: true,
-    deleted: false,
-    failure: true,
-    disableActions: false,
-    disableReactions: false,
-    files: [
-      {
-        name: 'My File',
-        size: 67351,
-        type: 'png',
-        audio: true,
-        duration: 14.4,
-        url: 'https://firebasestorage.googleapis.com/...',
-        preview: 'data:image/png;base64,iVBORw0KGgoAA...',
-        progress: 88
-      }
-    ],
-    reactions: {
-
+const currentUserId = ref("1234");
+const rooms = ref([
+	{
+		roomId: "1",
+		roomName: "Room 1",
+		avatar: "assets/imgs/people.png",
+		unreadCount: 4,
+		index: 3,
+		lastMessage: {
+			_id: "xyz",
+			content: "Last message received",
+			senderId: "1234",
+			username: "John Doe",
+			timestamp: "10:20",
+			saved: true,
+			distributed: false,
+			seen: false,
+			new: true,
+		},
+		users: [
+			{
+				_id: "1234",
+				username: "John Doe",
+				avatar: "assets/imgs/doe.png",
+				status: {
+					state: "online",
+					lastChanged: "today, 14:30",
+				},
+			},
+			{
+				_id: "4321",
+				username: "John Snow",
+				avatar: "assets/imgs/snow.png",
+				status: {
+					state: "offline",
+					lastChanged: "14 July, 20:00",
+				},
+			},
+		],
+		typingUsers: [4321],
 	},
-    replyMessage: {
-      content: 'Reply Message',
-      senderId: '4321',
-      files: [
-        {
-          name: 'My Replied File',
-          size: 67351,
-          type: 'png',
-          audio: true,
-          duration: 14.4,
-          url: 'https://firebasestorage.googleapis.com/...',
-          preview: 'data:image/png;base64,iVBORw0KGgoAA...'
-        }
-      ]
-    },
-  }
-])
-const roomActions =  ref([
-          { name: 'inviteUser', title: 'Invite User' },
-          { name: 'removeUser', title: 'Remove User' },
-          { name: 'deleteRoom', title: 'Delete Room' }
-        ])
+]);
+const messages = ref([
+	{
+		_id: "7890",
+		indexId: 12092,
+		content: "Message 1",
+		senderId: "1234",
+		username: "John Doe",
+		avatar: "assets/imgs/doe.png",
+		date: "13 November",
+		timestamp: "10:20",
+		system: false,
+		saved: true,
+		distributed: true,
+		seen: true,
+		deleted: false,
+		failure: true,
+		disableActions: false,
+		disableReactions: false,
+		files: [
+			{
+				name: "My File",
+				size: 67351,
+				type: "png",
+				audio: true,
+				duration: 14.4,
+				url: "https://firebasestorage.googleapis.com/...",
+				preview: "data:image/png;base64,iVBORw0KGgoAA...",
+				progress: 88,
+			},
+		],
+		reactions: {},
+		replyMessage: {
+			content: "Reply Message",
+			senderId: "4321",
+			files: [
+				{
+					name: "My Replied File",
+					size: 67351,
+					type: "png",
+					audio: true,
+					duration: 14.4,
+					url: "https://firebasestorage.googleapis.com/...",
+					preview: "data:image/png;base64,iVBORw0KGgoAA...",
+				},
+			],
+		},
+	},
+]);
+const roomActions = ref([
+	{ name: "inviteUser", title: "Invite User" },
+	{ name: "removeUser", title: "Remove User" },
+	{ name: "deleteRoom", title: "Delete Room" },
+]);
 
 // $("#toast").toast();
 </script>
 
 <template>
-	<header>Home</header>
-
 	<div class="container"></div>
 
 	<template>
-		<vue-advanced-chat
-		  :current-user-id="currentUserId"
-		  :rooms="JSON.stringify(rooms)"
-		  :messages="JSON.stringify(messages)"
-		  :room-actions="JSON.stringify(roomActions)"
-		/>
-	  </template>
+		<div class="row">
+			<div class="col-1 removePadding">
+				<div class="row serverIconRow removePadding" v-for="i in 10">
+					<img class="serverIcon" src="@/assets/testIcon.jpg" />
+				</div>
+			</div>
+			<div class="col-10 removePadding">
+				<vue-advanced-chat
+					theme="dark"
+					height="100vh"
+					:current-user-id="currentUserId"
+					:rooms="JSON.stringify(rooms)"
+					:messages="JSON.stringify(messages)"
+					:room-actions="JSON.stringify(roomActions)"
+				/>
+			</div>
+		</div>
+	</template>
 </template>
 
 <style scoped>
-
-
+@import "home.scss";
 </style>
