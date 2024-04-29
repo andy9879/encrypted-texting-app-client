7650b12273a9f27645a887a6f83abcce003555f1
Andrew Baumheckel <andrew.baumheckel@gmail.com>
2024-04-29 13:46:49 -0700

    Changed Add Friend Styling

--- src/components/addFriend/addFriend.vue
+++ src/components/addFriend/addFriend.vue
@@ -1,26 +1,39 @@
 <script setup>
 import { ref } from "vue";
+import { socket } from "@/scripts/socket";
 
-const count = ref(0);
+const searchQurry = ref("");
+
+//TODO Add min username size to search
+function searchForUser() {
+	socket.emit("findUser", searchQurry.value);
+}
 </script>
 
 <template>
 	<div class="content">
 		<div class="row">
-			<div class="col-8">
-				<b-input class="textInput"></b-input>
+			<div class="col-6">
+				<div class="row">
+					<div class="col">
+						<h3>Add Friend</h3>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-8">
+						<b-input class="textInput"></b-input>
+					</div>
+					<div class="col-4 buttonWrapper">
+						<b-icon class="button" icon="search" width="50px" height="30px"></b-icon>
+					</div>
+				</div>
 			</div>
-			<div class="col-2">
-				<b-button>Search</b-button>
-			</div>
-		</div>
-		<div class="row result">
-			<div class="col-1">
-				<img class="resultIcon" src="@/assets/testIcon2.jpg" />
-			</div>
-			<div class="col-7 resultText" style="diplay">Username or something</div>
-			<div class="col-2">
-				<b-button>Add</b-button>
+			<div class="col-6">
+				<div class="row">
+					<div class="col">
+						<h3>Pending Friend Requests</h3>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
