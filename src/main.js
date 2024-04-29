f6f1daa5d7eb17c7ddbb9799b3049d4e63908294
Andrew Baumheckel <andrew.baumheckel@gmail.com>
2024-04-28 18:01:41 -0700

    made bootstrap themeing work

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue, { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import App from "./App.vue";
 import router from "./router";
