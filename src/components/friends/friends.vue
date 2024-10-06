9ec55f9dcfe7b2817a1e26272f7d8b092d832160
Andrew Baumheckel <andrew.baumheckel@gmail.com>
2024-10-06 04:47:15 -0700

    temp

--- src/components/friends/friends.vue
+++ src/components/friends/friends.vue
@@ -6,11 +6,24 @@ socketGlobalListeners();
 
 import { useServerDataStore } from "@/stores/serverData";
 
+import { useClientDataStore } from "@/stores/clientData";
+
 let serverData = useServerDataStore();
+
+let clientData = useClientDataStore();
+
 </script>
 
 <template>
-	<div class="content">friends</div>
+	<div class="content">
+		<h1>Friends</h1>
+		<hr>
+		<div class="friends">
+			<div v-for="friend in clientData.data.friends" :key="friend.username" class="row">
+				<div class="col-1"><img width="64px" height="64px" src="@/assets/testIcon.jpg"></div><div class="col-4">Username</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
