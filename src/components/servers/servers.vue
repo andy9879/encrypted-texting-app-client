1ce23aefb50878bebe5cd299e2994a2f3fd6bcbf
Andrew B <andrew.baumheckel@gmail.com>
2025-11-22 19:14:27 -0800

    added some server page structure

--- src/components/servers/servers.vue
+++ src/components/servers/servers.vue
@@ -10,7 +10,14 @@ let serverData = useServerDataStore();
 </script>
 
 <template>
-	<div class="content">Servers</div>
+	<div class="content">
+		<div class="row">
+			<div class="col-1 server-col">
+				<img width="48px" src="@/assets/testIcon.jpg" />
+			</div>
+			<div class="col-11">server chats</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
