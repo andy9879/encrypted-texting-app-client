ede79b9ea2f253d4c34b4bf2cade553ce7860380
Andrew B <andrew.baumheckel@gmail.com>
2025-11-23 22:48:04 -0800

    changed chat input style

--- src/components/chatInterfaceInput/chatInterfaceInput.vue
+++ src/components/chatInterfaceInput/chatInterfaceInput.vue
@@ -1,32 +1,48 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import Quill from "/node_modules/quill/dist/quill.js";
+import Container from "quill/blots/container";
 
 onMounted(() => {
 	const quill = new Quill("#editor", {
 		theme: "snow",
 		modules: {
 			// Equivalent to { toolbar: { container: '#toolbar' }}
-			toolbar: "#toolbar",
+			toolbar: {
+				container: "#toolbar",
+			},
 		},
 	});
 });
 </script>
 
 <template>
-	<div>
-		<!-- make sure to densentize text data on the server side -->
-		<!-- Create the toolbar container -->
-		<div id="toolbar">
-			<button class="ql-bold">Bold</button>
-			<button class="ql-italic">Italic</button>
-		</div>
-
-		<!-- Create the editor container -->
+	<!-- TODO Fix toolBar -->
+	<div id="toolbar" style="display: none">
+		<!-- Add font size dropdown -->
+		<select class="ql-size">
+			<option value="small"></option>
+			<!-- Note a missing, thus falsy value, is used to reset to default -->
+			<option selected></option>
+			<option value="large"></option>
+			<option value="huge"></option>
+		</select>
+		<!-- Add a bold button -->
+		<button class="ql-bold"></button>
+		<!-- Add subscript and superscript buttons -->
+		<button class="ql-script" value="sub"></button>
+		<button class="ql-script" value="super"></button>
+	</div>
+	<div class="editorWrapper">
+		<!--TODO make sure to densentize text data on the server side -->
+		<!-- TODO fix chat input one word wrapping -->
 		<div id="editor">
 			<p>Hello World!</p>
-			<p>Some initial <strong>bold</strong> text</p>
-			<p><br /></p>
+		</div>
+		<div>
+			<div class="editorIcon pi pi-paperclip"></div>
+			<div class="editorIcon pi pi-image"></div>
+			<div class="editorIcon pi pi-send"></div>
 		</div>
 	</div>
 </template>
