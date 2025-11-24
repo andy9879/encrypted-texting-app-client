361293f411ac28e719ecf2479b7b75a9ecc3f539
Andrew B <andrew.baumheckel@gmail.com>
2025-11-23 21:01:38 -0800

    added todo

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 
+//TODO only import used icons
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
