1cf6b4fa49a5a2134c40f139e7deaa62892e3823
Andrew B <andrew.baumheckel@gmail.com>
2025-12-04 04:48:43 -0800

    updated sanitize

--- src/scripts/sanitize.js
+++ src/scripts/sanitize.js
@@ -2,11 +2,26 @@ import sanitizeHtml from "sanitize-html";
 
 export function sanitize(html) {
 	return sanitizeHtml(html, {
-		allowedTags: ["b", "i", "em", "u", "strong", "a", "img", "p"],
+		allowedTags: [
+			"b",
+			"i",
+			"em",
+			"u",
+			"strong",
+			"a",
+			"img",
+			"p",
+			"sup",
+			"sub",
+			"div",
+		],
 		allowedAttributes: {
 			a: ["href"],
 			img: ["src", "srcset"],
 		},
 		allowedSchemes: ["data", "http"],
+		allowedClasses: {
+			div: ["ql-code-block-container", "ql-code-block"],
+		},
 	}).replaceAll("<img ", '<img style="max-width: 15cm;" ');
 }
